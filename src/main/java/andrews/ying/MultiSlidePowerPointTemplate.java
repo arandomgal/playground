b78a28de763fac86abc234910fe29b035d9a7cbe
ying.andrews <ying.andrews@gmail.com>
2023-12-19 13:14:26 -0500

    java.util.ConcurrentModificationException investigation.

--- src/main/java/andrews/ying/MultiSlidePowerPointTemplate.java
+++ src/main/java/andrews/ying/MultiSlidePowerPointTemplate.java
@@ -1,4 +1,23 @@
 package andrews.ying;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiSlidePowerPointTemplate {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        System.out.println("before: " + integers);
+
+//        for (Integer integer : integers) {
+//            integers.remove(1);
+//        }
+        for (int i = 0; i < integers.size(); i++) {
+            integers.remove(1);
+        }
+
+        System.out.println("after: " + integers);
+    }
 }
